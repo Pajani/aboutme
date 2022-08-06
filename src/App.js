671ff8c305fd59228e4bcc2b44a51efb0e5bd22a
Pajani <pajani.arjunan@gmail.com>
2022-08-06 11:57:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Pajani Arjunan</h1>
+      <h2>Dotnet Tech Lead/Senior Developer!</h2>
+    </div>
+  );
+}
